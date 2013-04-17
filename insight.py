f5d165b488a65aec1ab580006272e1548e2b5ebc
Jyri Tuulos <team+foobar@bitdeli.com>
2013-04-17 03:59:06 +0000

    show top countries as a bar chart

--- insight.py
+++ insight.py
@@ -1,13 +1,31 @@
+from collections import Counter
+from bitdeli import textutil
 from bitdeli.insight import insight
-from bitdeli.widgets import Text, Map
+from bitdeli.widgets import Text, Bar, Map
+
+TOP_COUNT = 10
+
+def country_label(ccode):
+    name = textutil.country_name(ccode)
+    return "%s (%s)" % (name, ccode) if name else ccode
 
 @insight
-def view(model, params):    
+def view(model, params):
     yield Text(size=(12, 'auto'),
                label='Showing all users',
                data={'text': "## What is the geographic distribution of users?\n"})
 
+    countries = Counter({ccode: len(uids)
+                         for ccode, uids in model.items()})
+    
     yield Map(id='map',
-              size=(12, 6),
-              data={ccode: len(uids)
-                    for (ccode, uids) in dict(model).iteritems()})
+          size=(12, 6),
+          data=countries)
+    
+    yield Bar(id='top_countries',
+              label='Top %d countries' % TOP_COUNT,
+              size=(12, 4),
+              segmentable=False,
+              data=[(country_label(ccode), count)
+                    for ccode, count in countries.most_common(TOP_COUNT)])
+    
