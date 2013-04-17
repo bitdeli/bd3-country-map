335919096c231fce80fb818cea28e66144ba60a5
Jyri Tuulos <ville+ar2@bitdeli.com>
2013-04-17 05:12:30 +0000

    geocode event ip addresses to get country information

--- jsapi/model.py
+++ jsapi/model.py
@@ -1,4 +1,7 @@
 from bitdeli.model import model
+import GeoIP
+
+geoip = GeoIP.open('/usr/share/geoip/GeoLiteCity.dat', GeoIP.GEOIP_STANDARD)
 
 def latest_country(events):
     for tstamp, group, ip, event in events:
@@ -6,6 +9,10 @@ def latest_country(events):
             return event['geo_country_code']
         elif 'facebook_country' in event:
             return event['facebook_country']
+        elif 'ip' in event:
+            return geoip.record_by_addr(event['ip'])
+        else:
+            return geoip.record_by_addr(ip)
 
 @model
 def build(profiles):
