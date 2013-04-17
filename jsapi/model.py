2733bb7308fb6ce3d8eec966209f64a6ec3eafe1
Jyri Tuulos <ville+ar2@bitdeli.com>
2013-04-17 07:40:21 +0000

    add debug prints to country code parsing

--- jsapi/model.py
+++ jsapi/model.py
@@ -6,10 +6,13 @@ geoip = GeoIP.open('/usr/share/geoip/GeoLiteCity.dat', GeoIP.GEOIP_STANDARD)
 def latest_country(events):
     for tstamp, group, ip, event in events:
         if 'geo_country_code' in event:
+            print 'geo_country_code: %s' % event['geo_country_code']
             return event['geo_country_code']
         elif 'facebook_country' in event:
+            print 'facebook_country: %s' % event['facebook_country']
             return event['facebook_country']
         elif 'ip' in event:
+            print 'ip: %s' % geoip.record_by_addr(event['ip'])
             return geoip.record_by_addr(event['ip'])
         else:
             return geoip.record_by_addr(ip)
